866ef8940542d98fb560acb67e2c877e93cc2574
faatima_abidi <faatima.abidi24@gmail.com>
2024-09-24 23:39:13 -0400

    Completed Activity 1.3 by following example 3 in the textbook by adding templates and rendering them in hello.py.

--- hello.py
+++ hello.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+# ...
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 # home page
 @app.route('/')
 def index():
- return '<h1>Hello World!</h1>'
-# user page with name of user
+ return render_template('index.html')
+# user page
 @app.route('/user/<name>')
 def user(name):
- return '<h1>Hello, {}!</h1>'.format(name)
+ return render_template('user.html', name=name, current_time = datetime.utcnow())
